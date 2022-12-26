f6aa28ee476ae374637347e8104967e2475b9feb
Joseph Chen <jchen42703@gmail.com>
2022-12-26 13:51:24 -0500

    feat(echo-api): added random protected endpoint

--- echo-api/controllers/baseRoutes/router.go
+++ echo-api/controllers/baseRoutes/router.go
@@ -23,7 +23,20 @@ func GetSession() echo.HandlerFunc {
 	}
 }
 
+// Random protected endpoint
+func GetProtected() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		session, ok := c.Get(middleware.ORY_SESSION_CONTEXT_KEY).(*ory.Session)
+		if !ok || session == nil {
+			return c.JSON(http.StatusUnauthorized, templates.NewError(fmt.Errorf("must be signed in")))
+		}
+
+		return c.JSON(http.StatusOK, "protected")
+	}
+}
+
 // Test routes to see session data.
 func RegisterRoutes(g *echo.Group) {
 	g.GET("/session", GetSession())
+	g.GET("/protected", GetProtected())
 }
